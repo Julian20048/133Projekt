e7d7eac53b6439c9cf7377a93c5f3ac92b725945
Julian Janik <janikj@bzz.ch>
2022-05-18 12:56:58 +0200

    initial commit

---
+++ src/main/java/model/Spiel.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Alles was der Spieler während dem Spiel tut
+ */
+public class Spiel {
+    private int spielzeit;
+    private int erzTore;
+    private int erzAssists;
+    private int gelbeKarten;
+    private int roteKarten;
+    private int erfolgPässe;
+    private List<Spieler> spieler;
+
+    public int getSpielzeit() {
+        return spielzeit;
+    }
+
+    public void setSpielzeit(int spielzeit) {
+        this.spielzeit = spielzeit;
+    }
+
+    public int getErzTore() {
+        return erzTore;
+    }
+
+    public void setErzTore(int erzTore) {
+        this.erzTore = erzTore;
+    }
+
+    public int getErzAssists() {
+        return erzAssists;
+    }
+
+    public void setErzAssists(int erzAssists) {
+        this.erzAssists = erzAssists;
+    }
+
+    public int getGelbeKarten() {
+        return gelbeKarten;
+    }
+
+    public void setGelbeKarten(int gelbeKarten) {
+        this.gelbeKarten = gelbeKarten;
+    }
+
+    public int getRoteKarten() {
+        return roteKarten;
+    }
+
+    public void setRoteKarten(int roteKarten) {
+        this.roteKarten = roteKarten;
+    }
+
+    public int getErfolgPässe() {
+        return erfolgPässe;
+    }
+
+    public void setErfolgPässe(int erfolgPässe) {
+        this.erfolgPässe = erfolgPässe;
+    }
+
+    public List<Spieler> getSpieler() {
+        return spieler;
+    }
+
+    public void setSpieler(List<Spieler> spieler) {
+        this.spieler = spieler;
+    }
+}
---
+++ src/main/java/model/Spieler.java
@@ -0,0 +1,53 @@
+package model;
+
+/**
+ * Spieler, der das Spiel spielt
+ */
+public class Spieler {
+    private String name;
+    private String nationalität;
+    private int spielerNr;
+    private int alter;
+    private String position;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationalität() {
+        return nationalität;
+    }
+
+    public void setNationalität(String nationalität) {
+        this.nationalität = nationalität;
+    }
+
+    public int getSpielerNr() {
+        return spielerNr;
+    }
+
+    public void setSpielerNr(int spielerNr) {
+        this.spielerNr = spielerNr;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
