766b12a95dd6e9cb24369394ac9594a89a46e4d1
Julian Janik <janikj@bzz.ch>
2022-05-24 10:08:57 +0200

    initial commit

--- src/main/java/ch/bzz/KaderVerwaltung/service/TestService.java
+++ src/main/java/ch/bzz/KaderVerwaltung/service/TestService.java
@@ -1,13 +1,11 @@
 package ch.bzz.KaderVerwaltung.service;
 
 
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 
 
 /**
@@ -16,14 +14,14 @@ import javax.ws.rs.core.Response;
 @Path("test")
 public class TestService {
 
-
-
+    /**
+     * confirms the application runs
+     * @return message
+     */
     @GET
     @Path("test")
     @Produces(MediaType.TEXT_PLAIN)
     public Response test() {
-
-
 
         return Response
                 .status(200)
